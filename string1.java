82e0e76e204339c54eb0316b9f2bae87bd7edb43
TEJAS MS <tejastejasms71@gmail.com>
2025-08-29 19:19:22 +0530

    Add files via upload

---
+++ string1.java
@@ -0,0 +1,12 @@
+public class string1 {
+    public static void main(String[] args) {
+        System.out.println(56);
+        System.out.println("hi");
+        System.out.println(new int[] {2,3,4,5});
+        String name=null;
+        System.out.println(name);
+        
+    }
+
+    
+}
