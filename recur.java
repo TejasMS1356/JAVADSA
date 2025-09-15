67b9cd5fbb71386c36003d2fb1d5a0dfe48ae2a1
TEJAS MS <tejastejasms71@gmail.com>
2025-09-15 22:21:39 +0530

    Add files via upload

---
+++ recur.java
@@ -0,0 +1,15 @@
+public class recur {
+    public static void main(String[] args) {
+        print(1);    
+    }
+   static void print(int n){
+    if(n>=5){
+         System.out.println(n);//base condition
+        return;
+       
+    }
+        System.out.println(n);
+        print(n+1);
+   }
+    
+}
