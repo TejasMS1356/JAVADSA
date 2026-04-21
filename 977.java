f7a85161edffa14233aeceb28d7a052d1ae1ed2d
TEJAS MS <tejastejasms71@gmail.com>
2026-04-21 19:59:25 +0530

    Optimize sortedSquares method for better performance

--- 977.java
+++ 977.java
@@ -1,19 +1,19 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        for(int i=0;i<nums.length;i++){
-            nums[i]=nums[i]*nums[i];
-        }
-         for(int i=0;i<nums.length-1;i++){
-            for(int j=0;j<nums.length-1-i;j++){
-                if(nums[j]>nums[j+1]){
-                    int temp=nums[j];
-                    nums[j]=nums[j+1];
-                    nums[j+1]=temp;
-
-                }
+        int  i=0;
+        int j=nums.length-1;
+        int[] r = new int[nums.length];
+        for(int k=nums.length-1;k>=0;k--){
+            if(Math.abs(nums[i])>Math.abs(nums[j])){
+                r[k]=nums[i]*nums[i];
+                i++;
             }
-         }
-            return nums;
+            else{
+                r[k]=nums[j]*nums[j];
+                j--;
+            }
+        }
+        return r;
         
     }
 }
