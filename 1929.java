0e4d971ac1b0d10c27f16d75f17d08b5d48c1b45
TEJAS MS <tejastejasms71@gmail.com>
2026-04-26 14:20:12 +0530

    Create 1929.java

---
+++ 1929.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        
+       
+        int[] s=new int[nums.length*2];
+        
+       for (int i = 0; i < nums.length; i++) {
+            s[i]=nums[i];
+            
+            
+        }
+        for(int i=0;i<nums.length;i++){
+            s[i+nums.length]=nums[i];
+        }
+       return s;
+        
+    }
+}
