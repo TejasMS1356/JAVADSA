8c13b8b3cbddd8dddfee3d1789e92f84a47c6918
TEJAS MS <tejastejasms71@gmail.com>
2026-05-14 23:32:52 +0530

    Create 961.java

---
+++ 961.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+         HashMap<Integer,Integer> map=new HashMap<>();
+         for(int i=0;i<nums.length;i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+         }
+         int ans=0;
+         int n=(nums.length/2);
+         for(int i=0;i<nums.length;i++){
+            if(map.get(nums[i])==n){
+              ans=nums[i];  
+            }
+         }
+        return ans;
+    }
+}
